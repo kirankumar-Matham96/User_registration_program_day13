eedc4d005b9375e016c8b0cf017c16da9476140a
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-04-05 11:16:54 +0530

    ADD: Validating first name given by the user

--- main/java/comUserValidation/UserValidationProgram.java
+++ main/java/comUserValidation/UserValidationProgram.java
@@ -1,9 +1,22 @@
 package comUserValidation;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserValidationProgram
 {
+    //UC1: valid first name
+    public static boolean validateFirstName(String first_name)
+    {
+        return Pattern.matches("^[A-Z][a-z]{2,}",first_name);
+    }
+
+    //main method
     public static void main(String[] args)
     {
-        System.out.println("Welcome to User Registration Program!");
+        Scanner sc = new Scanner(System.in);
+        String first_name = sc.nextLine();
+        boolean b = validateFirstName(first_name);
+        System.out.println(b);
     }
 }
