4e6b100643958f1b0f5fac6b4ec78bb627c25be4
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-04-05 21:17:55 +0530

    REFACTORED: Validating all the email samples

--- main/java/comUserValidation/UserValidationProgram.java
+++ main/java/comUserValidation/UserValidationProgram.java
@@ -20,7 +20,7 @@ public class UserValidationProgram
     //UC3: valid email
     public static boolean validateEmail(String email)
     {
-        return Pattern.matches("^(.+)@(.+)$",email);
+        return Pattern.matches("^(?!\\.)[A-Za-z0-9]+([._%+-]?[0-9])*@[A-Za-z0-9-]+\\.[a-zA-Z]{2,}(\\.[A-Za-z]{2,})?$",email);
     }
 
     //UC4: valid phone number
@@ -45,8 +45,33 @@ public class UserValidationProgram
     {
         System.out.println("first name: "+validateFirstName("Kiran"));
         System.out.println("last name: "+validateFirstName("Kumar"));
-        System.out.println("Email: "+validateEmail("abc.xyz5@gmail.com.in"));
         System.out.println("Phone number: "+validatePhoneNumber("91 8688332960"));
         System.out.println("Password: "+validatePassword("dGs8j&*gh"));
+
+        //valid emails
+        System.out.println("Rahul@gmail.com: "+validateEmail("Rahul@gmail.com"));
+        System.out.println("Rahul-100@gmail.com: "+validateEmail("Rahul-100@gmail.com"));
+        System.out.println("Rahul.100@gmail.com: "+validateEmail("Rahul.100@gmail.com"));
+        System.out.println("Rahul-100@Rahul.com: "+validateEmail("Rahul-100@Rahul.com"));
+        System.out.println("Rahul-100@Rahul.net: "+validateEmail("Rahul-100@Rahul.net"));
+        System.out.println("Rahul.100@Rahul.com.au: "+validateEmail("Rahul.100@Rahul.com.au"));
+        System.out.println("Rahul@1.com: "+validateEmail("Rahul@1.com"));
+        System.out.println("Rahul@gmail.com.com: "+validateEmail("Rahul@gmail.com.com"));
+        System.out.println("Rahul+100@yahoo.com: "+validateEmail("Rahul+100@yahoo.com"));
+
+        //invalid emails
+        System.out.println("Rahul@.com.my: "+validateEmail("Rahul@.com.my"));
+        System.out.println("Rahul123@gmail.a: "+validateEmail("Rahul123@gmail.a"));
+        System.out.println("Rahul123@.com: "+validateEmail("Rahul123@.com"));
+        System.out.println("Rahul123@.com.com: "+validateEmail("Rahul123@.com.com"));
+        System.out.println(".Rahul@Rahul.com: "+validateEmail(".Rahul@Rahul.com"));
+        System.out.println("Rahul()*@gmail.com: "+validateEmail("Rahul()*@gmail.com"));
+        System.out.println("Rahul@%*.com: "+validateEmail("Rahul@%*.com"));
+        System.out.println("Rahul..2002@gmail.com: "+validateEmail("Rahul..2002@gmail.com"));
+        System.out.println("Rahul.@gmail.com: "+validateEmail("Rahul.@gmail.com"));
+        System.out.println("Rahul@Rahul@gmail.com: "+validateEmail("Rahul@Rahul@gmail.com"));
+        System.out.println("Rahul@gmail.com.1a: "+validateEmail("Rahul@gmail.com.1a"));
+        System.out.println("Rahul@gmail.com.aa.au: "+validateEmail("Rahul@gmail.com.aa.au"));
+
     }
 }
