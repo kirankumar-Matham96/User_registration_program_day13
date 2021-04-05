5f114e3e6c42404511e1ce95a5db4e7ef9e38f41
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-04-05 11:05:45 +0530

    ADD: welcome message

---
+++ main/java/comUserValidation/UserValidationProgram.java
@@ -0,0 +1,9 @@
+package comUserValidation;
+
+public class UserValidationProgram
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to User Registration Program!");
+    }
+}
