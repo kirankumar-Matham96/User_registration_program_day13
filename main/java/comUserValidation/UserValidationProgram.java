1a7c60d421f8b11da348ac0dd0a231655e528e2c
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-04-05 11:45:41 +0530

    ADD: Validating last name given by the user

--- main/java/comUserValidation/UserValidationProgram.java
+++ main/java/comUserValidation/UserValidationProgram.java
@@ -11,12 +11,23 @@ public class UserValidationProgram
         return Pattern.matches("^[A-Z][a-z]{2,}",first_name);
     }
 
+    //UC1: valid last name
+    public static boolean validateLastName(String last_name)
+    {
+        return Pattern.matches("^[A-Z][a-z]{2,}",last_name);
+    }
+
     //main method
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first name");
         String first_name = sc.nextLine();
-        boolean b = validateFirstName(first_name);
-        System.out.println(b);
+        System.out.println("Enter last name");
+        String last_name = sc.nextLine();
+
+        System.out.println(validateFirstName(first_name));
+        System.out.println(validateFirstName(last_name));
+
     }
 }
