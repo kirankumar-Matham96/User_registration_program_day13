6725ddbe1edd6cfe8c13894d0a86fcda8ec30998
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-04-05 20:41:18 +0530

    ADD: Validating phone number

--- main/java/comUserValidation/UserValidationProgram.java
+++ main/java/comUserValidation/UserValidationProgram.java
@@ -23,12 +23,18 @@ public class UserValidationProgram
         return Pattern.matches("^(.+)@(.+)$",email);
     }
 
+    //UC4: valid phone number
+    public static boolean validatePhoneNumber(String phone_number)
+    {
+        return Pattern.matches("^[0-9]{1,2}\\s[0-9]{10}$",phone_number);
+    }
+
     //main method
     public static void main(String[] args)
     {
         System.out.println("first name: "+validateFirstName("Kiran"));
         System.out.println("last name: "+validateFirstName("Kumar"));
         System.out.println("Email: "+validateEmail("abc.xyz5@gmail.com.in"));
-
+        System.out.println("Phone number: "+validatePhoneNumber("91 8688332960"));
     }
 }
